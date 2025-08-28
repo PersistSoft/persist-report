2440ada9f77c7de7cad6478dc4e8966d04a1042c
Jose Luis Campos Bautista <jcampos@ontop.ai>
2025-08-28 17:54:06 -0600

    Configure deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,16 @@
-echo "Iniciando despliegue"
-#git pull
+echo"Starting deploy"
 
-CID=$(docker ps -aqf "name=report-server")
-echo $CID
+echo"Download changes"
+#git pull origin dev
 
-echo "Detendiendo contenedor"
-docker stop $CID
+echo"Stop containers"
+docker-compose down
 
-echo "Borrando contenedor"
-docker rm $CID
+echo"Delete images"
+docker rmi persist-reports-app
 
-echo "Borrando imagen"
-docker rmi report-server:v1
+echo"Create new images"
+docker-compose build
 
-echo "Creando jar de spring"
-mvn clean package -DskipTests -Dmaven.test.skip=true
-
-docker build -t report-server:v1 .
-docker run -p 8082:8082 --name report-server -d report-server:v1
-
-echo "Terminando despliegue"
+echo"Start containers"
+docker-compose up -d
